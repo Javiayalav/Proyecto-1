bf0c0a7a7f3dab00eca2d29e11533814417a929f
Javiayalav <129110877+Javiayalav@users.noreply.github.com>
2023-03-27 16:04:15 -0600

    Create currency.py

---
+++ currency.py
@@ -0,0 +1,11 @@
+#Final pr
+yuan = 560
+yen = 2455
+won = 3280
+usd = (yuan * 0.15) + (yen * 0.0076) + (won * 0.00077)
+#usd = 105.18
+print("what do you have left in yuan? 560")
+print("what do you have left in yen? 2455")
+print("what do you have left in won? 3280")
+
+print(usd)
