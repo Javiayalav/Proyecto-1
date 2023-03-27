02a29102b5ebaf80e52dfe191143c49ebfb81cbb
Javiayalav <129110877+Javiayalav@users.noreply.github.com>
2023-03-27 15:19:57 -0600

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,16 @@
+## Exponents
+score1 = 2**2  #score is 4
+score2 = 2**3  #score is 8
+score3 = 2**4  #score is 16
+score4 = 2**5  #score is 32
+
+print(score4)
+
+
+# Calculando mi Body mass index usando exponentes
+
+peso = 78.0
+altura = 1.80
+bmi = peso / (altura**2)
+print(bmi)
+
