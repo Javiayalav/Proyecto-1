088913bcf89179c2a36938e9059e77b40d36332c
Javiayalav <129110877+Javiayalav@users.noreply.github.com>
2023-03-27 15:04:27 -0600

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,28 @@
+# Operators
+score1 = 0       #score is 0
+score2= 4 + 3   #score is now 7
+score3 = 4 - 3   #score is now 1
+score4 = 4 * 3   #score is now 12
+score5 = 4 / 3   #score is now 1.3333
+score6 = 4 % 3   #score is now 1
+
+print (score2) 
+
+pizza = 2.99
+coke = 0.99
+total = pizza + coke
+tip = total * 0.2
+
+print(tip) #Output: 0.796
+
+tip = (pizza + coke) * 0.2 # Otra manera de sumar
+
+# Ejercicio de temperatura
+# Create a temperature.py program that converts a temperature from Fahrenheit (°F) to Celsius (°C).
+Fahrenheit = 49
+Celsius = (Fahrenheit - 32) / 1.8 
+print(Celsius)
+
+
+
+
